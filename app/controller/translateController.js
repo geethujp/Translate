5f4b70a8b892994c69f496e85a8afc75c07eae2c
geethuj <geethuj@qburst.com>
2016-06-29 11:41:05 +0530

    language switching enabled

--- app/controller/translateController.js
+++ app/controller/translateController.js
@@ -4,16 +4,15 @@
         .controller('translateCtrl', translateCtrl);
     translateCtrl.$inject = ["$scope", "$rootScope","$translate"];
 
-    function translateCtrl($scope, $rootScope) {
+    function translateCtrl($scope, $rootScope,$translate) {
         $scope.names = [
             { name: 'Jani', country: 'Norway' },
             { name: 'Hege', country: 'Sweden' },
             { name: 'Kai', country: 'Denmark' }
         ];
         console.log($scope.names);
+        $scope.changeLanguage = function (key) {
+            $translate.use(key);
+        };
     }
-
- $scope.changeLanguage = function (langKey) {
-        $translate.use(langKey);
-    };
 })();
